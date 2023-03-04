f5624890c3f91318a7e476673a98e59fabeb445f
MickMarch <michael.r.marchand@gmail.com>
2023-03-04 14:07:21 -0500

    Styled the Webpage

--- static/js/charts.js
+++ static/js/charts.js
@@ -105,7 +105,7 @@ function buildCharts(sample) {
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
-      title: "Top 10 Bacteria Cultures Found"
+      title: "<b>Top 10 Bacteria Cultures Found</b>"
     };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
@@ -129,7 +129,7 @@ function buildCharts(sample) {
 
     // Deliverable 2: 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-      title: "Bacteria Cultures Per Sample",
+      title: "<b>Bacteria Cultures Per Sample</b>",
       xaxis: {
         title: "OTU ID"
       },
